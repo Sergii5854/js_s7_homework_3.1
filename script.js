0f5804593c49cad21e03566968ed2fd8d0277eb5
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-23 11:27:07 +0300

    add formating

--- script.js
+++ script.js
@@ -6,33 +6,78 @@ console.info("Home Work v1.1")
 var test_array = [5, 4, 3, 2, 1]
 
 
-Object.prototype.myForEach = function( data ){
-	for ( i = 0; i <= this.length; i++ ) {
-	data(  this[i],  Object.prototype.toString.call(data) === '[object Array]' )
-	// console.log(i, data[i]);
-	}
+Object.prototype.myForEach = function (data) {
+    for (i = 0; i <= this.length; i++) {
+        data(this[i], Object.prototype.toString.call(data) === '[object Array]')
+        // console.log(i, data[i]);
+    }
 }
 
 console.group("my ForEach ");
 console.info(" ForEach")
-console.log( test_array )
+console.log(test_array)
 
- test_array.myForEach(function(data){
- 	console.log( data )
- })
+test_array.myForEach(function (data) {
+    console.log(data)
+})
 
 console.groupEnd();
 ///////////////////////////////////////////////////////
 
 
-  Array.prototype.myForEachPolyFill = function (callback, thisArg) {
+Array.prototype.myForEachPolyFill = function (callback, thisArg) {
 
     var T, k;
 
     if (this == null) {
-      throw new TypeError(' this is null or not defined');
+        throw new TypeError(' this is null or not defined');
     }
 
+    var O = Object(this);
+    var len = O.length >>> 0;
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+
+    if (arguments.length > 1) {
+        T = thisArg;
+    }
+
+    k = 0;
+
+    while (k < len) {
+
+        var kValue;
+        if (k in O) {
+            kValue = O[k];
+            callback.call(T, kValue, k, O);
+        }
+
+        k++;
+    }
+};
+
+
+console.group("my ForEach PolyFill");
+console.info(" ForEach")
+console.log(test_array)
+
+test_array.myForEachPolyFill(function (data) {
+    console.log(data)
+})
+
+console.groupEnd();
+
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+
+///////////////////////////////////////////////////////
+
+Array.prototype.myMap = function (callback, thisArg) {
+    var T, A, k;
+    if (this == null) {
+        throw new TypeError(' this is null or not defined');
+    }
     var O = Object(this);
 
     var len = O.length >>> 0;
@@ -42,105 +87,50 @@ console.groupEnd();
     }
 
     if (arguments.length > 1) {
-      T = thisArg;
+        T = thisArg;
     }
 
-    k = 0;
-
-    while (k < len) {
-
-      var kValue;
-
-      if (k in O) {
-
-      
-        kValue = O[k];
-
-        callback.call(T, kValue, k, O);
-      }
-
-      k++;
-    }
- 
-  };
-
-
-console.group("my ForEach PolyFill");
-console.info(" ForEach")
-console.log( test_array )
-
- test_array.myForEachPolyFill(function(data){
- 	console.log( data )
- })
- 
-console.groupEnd();
-
- // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-
-///////////////////////////////////////////////////////
-
-  Array.prototype.myMap = function(callback, thisArg) {
-  	var T, A, k;
-    if (this == null) {
-      throw new TypeError(' this is null or not defined');
-    }
-    var O = Object(this);
-
-    var len = O.length >>> 0;
-
-    if (typeof callback !== 'function') {
-      throw new TypeError(callback + ' is not a function');
-    }
-
-    if (arguments.length > 1) {
-      T = thisArg;
-    }
-
-    A = new Array(len);   
+    A = new Array(len);
     k = 0;
     while (k < len) {
-      var kValue, mappedValue;
-     
-      if (k in O) {
-      
-        kValue = O[k];
+        var kValue, mappedValue;
 
-        mappedValue = callback.call(T, kValue, k, O);
+        if (k in O) {
 
+            kValue = O[k];
+            mappedValue = callback.call(T, kValue, k, O);
+            A[k] = mappedValue;
+        }
 
-        A[k] = mappedValue;
-      }
-
-      k++;
+        k++;
     }
     return A;
-  };
+};
 
 console.group("my Map PolyFill");
 console.info(" map")
-console.log( test_array )
+console.log(test_array)
 
- test_array.myMap(function(data){
- 	console.log( data )
- })
+test_array.myMap(function (data) {
+    console.log(data)
+})
 
 console.groupEnd();
-
 
 
 ///////////////////////////////////////////////////////
 
 // http://www.stoimen.com/blog/2010/07/09/friday-algorithms-javascript-bubble-sort/
 
-Object.prototype.mySort = function(data) {
+Object.prototype.mySort = function (data) {
     var swapped
     do {
         swapped = false;
-        for (var i = 0; i < this.length-1; i++) {
-            if (this[i] > this[i+1]) {
+        for (var i = 0; i < this.length - 1; i++) {
+            if (this[i] > this[i + 1]) {
                 var temp = this[i]
-                this[i] = this[i+1]
-                this[i+1] = temp
+                this[i] = this[i + 1]
+                this[i + 1] = temp
                 swapped = true
             }
         }
@@ -150,7 +140,6 @@ Object.prototype.mySort = function(data) {
 test_array.mySort()
 console.group("bubble sort");
 console.info(" mySort")
-console.log( test_array )
+console.log(test_array)
 
 console.groupEnd();
-
