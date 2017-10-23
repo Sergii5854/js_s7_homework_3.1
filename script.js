014aa9747c71c3661a3e8f2efd84cbb10d12fdca
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-23 08:05:05 +0300

    add polyfill to myForEach

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ var test_array = [5, 4, 3, 2, 1]
 
 Object.prototype.myForEach = function( data ){
 	for ( i = 0; i <= this.length; i++ ) {
-	data(  this[i] )
+	data(  this[i],  Object.prototype.toString.call(data) === '[object Array]' )
 	// console.log(i, data[i]);
 	}
 }
 
  test_array.myForEach(function(data){
- 	console.log(data)
+ 	console.log( data )
  })
