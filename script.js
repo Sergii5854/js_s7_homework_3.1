367080bd131c351c1ec2d3f43dd2675f22694439
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-23 07:43:03 +0300

    updata myForEach

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 // ///////////////////////////
 // v1
 // ///////////////////////////
-console.info("Home Work v1.1");
+console.info("Home Work v1.1")
 
 var test_array = [5, 4, 3, 2, 1]
 
 
 Object.prototype.myForEach = function( data ){
 	for ( i = 0; i <= this.length; i++ ) {
-	data(  this[i] );
+	data(  this[i] )
 	// console.log(i, data[i]);
 	}
 }
 
-
-
  test_array.myForEach(function(data){
- 	console.log(data);
-
+ 	console.log(data)
  })
