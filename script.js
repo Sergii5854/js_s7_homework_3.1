c894c53a5048441c65c6078c7ba91a14062d7dbe
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-23 07:41:09 +0300

    add myForEach obj method

--- script.js
+++ script.js
@@ -3,7 +3,19 @@
 // ///////////////////////////
 console.info("Home Work v1.1");
 
+var test_array = [5, 4, 3, 2, 1]
+
+
+Object.prototype.myForEach = function( data ){
+	for ( i = 0; i <= this.length; i++ ) {
+	data(  this[i] );
+	// console.log(i, data[i]);
+	}
+}
 
 
 
- 
+ test_array.myForEach(function(data){
+ 	console.log(data);
+
+ })
