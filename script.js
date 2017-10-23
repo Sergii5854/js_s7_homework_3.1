7706628f63658fa4ed4210f2a922a09095446d49
Sergii Bondarenko <bondarenko.sergii.i@gmail.com>
2017-10-23 08:22:22 +0300

    add myForEach add polyfill

--- script.js
+++ script.js
@@ -18,6 +18,52 @@ Object.prototype.myForEach = function( data ){
  })
 
 
+
+
+
+  Array.prototype.myForEachPolyFill = function (callback, thisArg) {
+
+    var T, k;
+
+    if (this == null) {
+      throw new TypeError(' this is null or not defined');
+    }
+
+    var O = Object(this);
+
+    var len = O.length >>> 0;
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+
+    if (arguments.length > 1) {
+      T = thisArg;
+    }
+
+    k = 0;
+
+    while (k < len) {
+
+      var kValue;
+
+      if (k in O) {
+
+      
+        kValue = O[k];
+
+        callback.call(T, kValue, k, O);
+      }
+
+      k++;
+    }
+ 
+  };
+
+ test_array.myForEachPolyFill(function(data){
+ 	console.log( data )
+ })
+
  // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 
 
